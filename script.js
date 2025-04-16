143bb293afc4e1ffbe61a938982e1ae8b6f10c4f
Tanishq Sonwane <tanishqsonwane@example.com>
2025-04-16 12:43:21 +0530

    rotaion array k times

--- script.js
+++ script.js
@@ -69,3 +69,16 @@
 // console.log(ans)
 
 
+// let arr = [1,2,3,4,5]
+// let k = 2
+// k = k%arr.length
+// let count = 0
+// for(let j =1; j<=k;j++){
+//     count++
+//     let one = arr[0]
+//     for(let i =0; i<arr.length-1;i++){
+//         arr[i] = arr[i+1]
+//     }
+//     arr[arr.length-1] = one
+// }
+// console.log(arr,count)
